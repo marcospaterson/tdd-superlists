f7f20396f52792f5bb56e043dd61dd6198407287
nos-retap <marcos.paterson@gmail.com>
2020-05-05 21:48:03 -0400

    hack ft runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,3 +1,5 @@
+import os
+
 from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
@@ -13,6 +15,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):        
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
         
     def tearDown(self):
         self.browser.quit()
